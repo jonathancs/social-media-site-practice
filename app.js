90de34d62107bdc5ea341a5652670518fdd4c9b9
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-04-29 18:52:56 -0300

    4. what is a router

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 
+const router = require('./router')
+
 app.use(express.static('public'))
 app.set('views', 'views') // o 1o parametro precisa ser 'views' que estamos especificando as views, o 2o é o nome da pasta.
 app.set('view engine', 'ejs') // engine que vai renderizar as views
 
-app.get('/', function (req, res) {
-    res.render('home-guest')
-})
+app.use('/', router)
 
 app.listen(3000)
+
