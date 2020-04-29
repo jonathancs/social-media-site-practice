393f107458986634077af6a0c080a67c131f341e
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-04-29 19:08:27 -0300

    5. what is a controller

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+exports.login = function () {
+
+}
+
+exports.logout = function () {
+
+}
+
+exports.register = function (req, res) {
+    res.send('thaaankssss')
+}
+
+exports.home = function (req, res) {
+    res.render('home-guest')
+}
+
