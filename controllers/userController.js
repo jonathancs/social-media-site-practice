917d53af71871721699e5173299724c8571fab16
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-04-29 23:58:27 -0300

    8. adding validation to our model - incomplete

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,9 +9,13 @@ exports.logout = function () {
 }
 
 exports.register = function (req, res) {
-    let user = new user(req.body)
+    let user = new User(req.body)
     user.register()
-    res.send('thaaankssss')
+    if (user.errors.length) {
+        res.send(user.errors)
+    } else {
+        res.send("congrats, no errors")
+    } 
 }
 
 exports.home = function (req, res) {
