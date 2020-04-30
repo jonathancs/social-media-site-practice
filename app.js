75a854e4e6a55d9a8a438fa65e12ce24826896ab
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-04-29 23:58:26 -0300

    8. adding validation to our model - incomplete

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 
 const router = require('./router')
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
 app.use(express.static('public'))
 app.set('views', 'views') // o 1o parametro precisa ser 'views' que estamos especificando as views, o 2o é o nome da pasta.
 app.set('view engine', 'ejs') // engine que vai renderizar as views
