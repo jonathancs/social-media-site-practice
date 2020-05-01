4fc5b4610f7ac51996f5940daab846788d4a0f76
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-05-01 11:42:21 -0300

    13. Letting Users Log In

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,10 @@
 const User = require('../models/User')
 
-exports.login = function () {
-
+exports.login = function (req, res) {
+    let user = new User(req.body)
+    user.login(function (result) {  //provide a function as ARGUMENT porque não são sabemos quanto vai demorar
+        res.send(result)
+    }) // when to send back the response. WHEN ?   traditionally: callback;  modern: promises    
 }
 
 exports.logout = function () {
