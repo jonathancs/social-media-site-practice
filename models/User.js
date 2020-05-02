b5e6bf7b1a3cd5ebacdaf921e6485c842e5f3b27
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-05-02 14:54:58 -0300

    17. Hashing User Passwords

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs")
 const usersCollection = require('../db').collection("users")
 const validator = require("validator")
 
@@ -26,7 +27,7 @@ User.prototype.validate = function () {
     if (!validator.isEmail(this.data.email)) {this.errors.push("you must provide a valid email")}
     if (this.data.password == "") {this.errors.push("you must provide a password")}
     if (this.data.password.length > 0 && this.data.password.length < 12) {this.errors.push("password must be at least 12 characters")}
-    if (this.data.password.length > 100) {this.errors.push("password cannot exceed 100 characters")}
+    if (this.data.password.length > 50) {this.errors.push("password cannot exceed 50 characters")}
     if (this.data.password.username > 0 && this.data.username.length < 3) {this.errors.push("Username must be at least 3 characters")}
     if (this.data.password.username < 100) {this.errors.push("username cannot exceed 100 characters")}
 }
@@ -35,12 +36,10 @@ User.prototype.login = function () {
     return new Promise((resolve, reject) => { 
         /*an anonymous function behave directing the THIS keyword 
         (on this.data.username) to the global scope.
-
         so we must change the function syntax to arrow function */
-        
         this.cleanUp()
         usersCollection.findOne({ username: this.data.username }).then((attemptedUser) => {
-            if (attemptedUser && attemptedUser.password == this.data.password) {
+            if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
                 resolve("congraaaaaaats")
             } else {
                 reject("invaliiiiiiiiiddd")
@@ -59,6 +58,9 @@ User.prototype.register = function () {
     // 2 - only if there are no validation errors
     // then save user data into database
     if (!this.errors.length) {
+        // hash user password
+        let salt = bcrypt.genSaltSync(10)
+        this.data.password = bcrypt.hashSync(this.data.password, salt)
         usersCollection.insertOne(this.data)
     }
 
