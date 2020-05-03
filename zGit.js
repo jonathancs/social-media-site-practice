5e0004b7e1c434ec8e68063f3fb690a313f79147
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-05-03 11:20:15 -0300

    Script para automatizar commits - zGit.js

--- zGit.js
+++ zGit.js
@@ -1,62 +1,102 @@
 
-let nomeDaAula = "20. Understanding Sessions"
+let comentarioGit = "Script para automatizar commits"
 
-let array1 = [
+let arquivosAlterados = [
     "zGit.js"
 ]
 
+if (arquivosAlterados.length == 1) {
+    arquivosAlterados.splice(1, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[0] + '"')
+    arquivosAlterados.splice(3, 0, 'git push')
+    arquivosAlterados.splice(5, 0, '')
+    arquivosAlterados[0] = 'git add "' + arquivosAlterados[0] + '"';
+    
 
+} else if (arquivosAlterados.length == 2) {
+    arquivosAlterados.splice(1, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[0] + '"')
+    arquivosAlterados.splice(3, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[2] + '"')
+    arquivosAlterados.splice(5, 0, 'git push')
+    arquivosAlterados.splice(7, 0, '')
+    arquivosAlterados[0] = 'git add "' + arquivosAlterados[0] + '"';
+    arquivosAlterados[2] = 'git add "' + arquivosAlterados[2] + '"';
 
+} else if (arquivosAlterados.length == 3) {
+    arquivosAlterados.splice(1, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[0] + '"')
+    arquivosAlterados.splice(3, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[2] + '"')
+    arquivosAlterados.splice(5, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[4] + '"')
+    arquivosAlterados.splice(7, 0, 'git push')
+    arquivosAlterados.splice(9, 0, '')
+    arquivosAlterados[0] = 'git add "' + arquivosAlterados[0] + '"';
+    arquivosAlterados[2] = 'git add "' + arquivosAlterados[2] + '"';
+    arquivosAlterados[4] = 'git add "' + arquivosAlterados[4] + '"';
 
+} else if (arquivosAlterados.length == 4) {
+    arquivosAlterados.splice(1, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[0] + '"')
+    arquivosAlterados.splice(3, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[2] + '"')
+    arquivosAlterados.splice(5, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[4] + '"')
+    arquivosAlterados.splice(7, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[6] + '"')
+    arquivosAlterados.splice(9, 0, 'git push')
+    arquivosAlterados.splice(11, 0, '')
+    arquivosAlterados[0] = 'git add "' + arquivosAlterados[0] + '"';
+    arquivosAlterados[2] = 'git add "' + arquivosAlterados[2] + '"';
+    arquivosAlterados[4] = 'git add "' + arquivosAlterados[4] + '"';
+    arquivosAlterados[6] = 'git add "' + arquivosAlterados[6] + '"';
 
+} else if (arquivosAlterados.length == 5) {
+    arquivosAlterados.splice(1, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[0] + '"')
+    arquivosAlterados.splice(3, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[2] + '"')
+    arquivosAlterados.splice(5, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[4] + '"')
+    arquivosAlterados.splice(7, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[6] + '"')
+    arquivosAlterados.splice(9, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[8] + '"')
+    arquivosAlterados.splice(11, 0, 'git push')
+    arquivosAlterados.splice(13, 0, '')
+    arquivosAlterados[0] = 'git add "' + arquivosAlterados[0] + '"';
+    arquivosAlterados[2] = 'git add "' + arquivosAlterados[2] + '"';
+    arquivosAlterados[4] = 'git add "' + arquivosAlterados[4] + '"';
+    arquivosAlterados[6] = 'git add "' + arquivosAlterados[6] + '"';
+    arquivosAlterados[8] = 'git add "' + arquivosAlterados[8] + '"';
 
+} else if (arquivosAlterados.length == 6) {
+    arquivosAlterados.splice(1, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[0] + '"')
+    arquivosAlterados.splice(3, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[2] + '"')
+    arquivosAlterados.splice(5, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[4] + '"')
+    arquivosAlterados.splice(7, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[6] + '"')
+    arquivosAlterados.splice(9, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[8] + '"')
+    arquivosAlterados.splice(11, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[10] + '"')
+    arquivosAlterados.splice(13, 0, 'git push')
+    arquivosAlterados.splice(15, 0, '')
+    arquivosAlterados[0] = 'git add "' + arquivosAlterados[0] + '"';
+    arquivosAlterados[2] = 'git add "' + arquivosAlterados[2] + '"';
+    arquivosAlterados[4] = 'git add "' + arquivosAlterados[4] + '"';
+    arquivosAlterados[6] = 'git add "' + arquivosAlterados[6] + '"';
+    arquivosAlterados[8] = 'git add "' + arquivosAlterados[8] + '"';
+    arquivosAlterados[10] = 'git add "' + arquivosAlterados[10] + '"';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-array1.splice(1, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[0] + '"')
-array1.splice(3, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[2] + '"')
-array1.splice(5, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[4] + '"')
-array1.splice(7, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[6] + '"')
-array1.splice(9, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[8] + '"')
-array1.splice(11, 0, 'git push')
-
-for (var i = 0; i < array1.length; i++) {
-    if(i % 2 === 0) { // index is even
-        array1[i] = 'git add "' + array1[i] + '"';
-    }
+} else if (arquivosAlterados.length == 7) {
+    arquivosAlterados.splice(1, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[0] + '"')
+    arquivosAlterados.splice(3, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[2] + '"')
+    arquivosAlterados.splice(5, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[4] + '"')
+    arquivosAlterados.splice(7, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[6] + '"')
+    arquivosAlterados.splice(9, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[8] + '"')
+    arquivosAlterados.splice(11, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[10] + '"')
+    arquivosAlterados.splice(13, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[12] + '"')
+    arquivosAlterados.splice(15, 0, 'git push')
+    arquivosAlterados.splice(17, 0, '')
+    arquivosAlterados[0] = 'git add "' + arquivosAlterados[0] + '"';
+    arquivosAlterados[2] = 'git add "' + arquivosAlterados[2] + '"';
+    arquivosAlterados[4] = 'git add "' + arquivosAlterados[4] + '"';
+    arquivosAlterados[6] = 'git add "' + arquivosAlterados[6] + '"';
+    arquivosAlterados[8] = 'git add "' + arquivosAlterados[8] + '"';
+    arquivosAlterados[10] = 'git add "' + arquivosAlterados[10] + '"';
+    arquivosAlterados[12] = 'git add "' + arquivosAlterados[12] + '"';
 }
 
-for (let i = 0; i < array1.length; i++) {
-    console.log(array1[i])    
+// for (var i = 0; i < arquivosAlterados.length; i++) {
+//     if (i % 2 === 0) { // index is even
+//         arquivosAlterados[i] = 'git add "' + arquivosAlterados[i] + '"';
+//     }
+// }
+
+for (let i = 0; i < arquivosAlterados.length; i++) {
+    console.log(arquivosAlterados[i])
 }
