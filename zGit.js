5070f40dc4c2e008c120015272d6d960fdd84ac7
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-05-03 10:50:24 -0300

    20. Understanding Sessions - zGit.js

--- zGit.js
+++ zGit.js
@@ -1,53 +1,62 @@
-// automate the commits attempt 1 - 2/5/20
+
+let nomeDaAula = "20. Understanding Sessions"
 
 let array1 = [
-    "caixa",
-    "amarela",
-    "usada"
+    "zGit.js"
 ]
 
-for (var i=0; i < array1.length; i++) {
-    array1[i] = 'git add "' + array1[i] + '"';
-}
 
-array1.splice(1, 0, 'git commit -m "nome da aula - ' + array1[0])
-array1.splice(3, 0, 'git commit -m "nome da aula - ' + array1[2])
-array1.splice(5, 0, 'git commit -m "nome da aula - ' + array1[4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+array1.splice(1, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[0] + '"')
+array1.splice(3, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[2] + '"')
+array1.splice(5, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[4] + '"')
+array1.splice(7, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[6] + '"')
+array1.splice(9, 0, 'git commit -m "' + nomeDaAula + ' - ' + array1[8] + '"')
+array1.splice(11, 0, 'git push')
+
+for (var i = 0; i < array1.length; i++) {
+    if(i % 2 === 0) { // index is even
+        array1[i] = 'git add "' + array1[i] + '"';
+    }
+}
 
 for (let i = 0; i < array1.length; i++) {
     console.log(array1[i])    
 }
-
-array1.splice(1, 0, 'git commit -m "nome da aula - ${nome do arquivo}"')
-
-
-
-/*
-
-git add ".gitignore"
-git commit -m "20. Understanding Sessions"
-
-git add "db.js"
-git commit -m "20. Understanding Sessions"
-
-git add "models/User.js"
-git commit -m "20. Understanding Sessions"
-
-git add "app.js"
-git commit -m "20. Understanding Sessions"
-
-git add "controllers/userController.js"
-git commit -m "20. Understanding Sessions"
-
-git add "zGit"
-git commit -m "20. Understanding Sessions"
-
-git add "package-lock.json"
-git commit -m "20. Understanding Sessions"
-
-git add "package.json"
-git commit -m "20. Understanding Sessions"
-
-git push
-
-*/
