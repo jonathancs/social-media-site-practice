be213ec02b4e778d30833189c3fcef435ed3aec4
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-05-04 23:07:16 -0300

    22. Adding Flash Messages - zGit.js

--- zGit.js
+++ zGit.js
@@ -1,12 +1,15 @@
 
-let comentarioGit = "21. Letting Users Logout"
-
+let comentarioGit = "22. Adding Flash Messages"
 
 let arquivosAlterados = [
     "controllers/userController.js",
     "router.js",
     "views/home-dashboard.ejs",
-    "zGit.js"
+    "zGit.js",
+    "app.js",
+    "package-lock.json",
+    "package.json",
+    "views/home-guest.ejs"
 
 ]
 
@@ -94,6 +97,26 @@ if (arquivosAlterados.length == 1) {
     arquivosAlterados[8] = 'git add "' + arquivosAlterados[8] + '"';
     arquivosAlterados[10] = 'git add "' + arquivosAlterados[10] + '"';
     arquivosAlterados[12] = 'git add "' + arquivosAlterados[12] + '"';
+
+} else if (arquivosAlterados.length == 8) {
+    arquivosAlterados.splice(1, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[0] + '"')
+    arquivosAlterados.splice(3, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[2] + '"')
+    arquivosAlterados.splice(5, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[4] + '"')
+    arquivosAlterados.splice(7, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[6] + '"')
+    arquivosAlterados.splice(9, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[8] + '"')
+    arquivosAlterados.splice(11, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[10] + '"')
+    arquivosAlterados.splice(13, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[12] + '"')
+    arquivosAlterados.splice(15, 0, 'git commit -m "' + comentarioGit + ' - ' + arquivosAlterados[14] + '"')
+    arquivosAlterados.splice(17, 0, 'git push')
+    arquivosAlterados.splice(19, 0, '')
+    arquivosAlterados[0] = 'git add "' + arquivosAlterados[0] + '"';
+    arquivosAlterados[2] = 'git add "' + arquivosAlterados[2] + '"';
+    arquivosAlterados[4] = 'git add "' + arquivosAlterados[4] + '"';
+    arquivosAlterados[6] = 'git add "' + arquivosAlterados[6] + '"';
+    arquivosAlterados[8] = 'git add "' + arquivosAlterados[8] + '"';
+    arquivosAlterados[10] = 'git add "' + arquivosAlterados[10] + '"';
+    arquivosAlterados[12] = 'git add "' + arquivosAlterados[12] + '"';
+    arquivosAlterados[14] = 'git add "' + arquivosAlterados[14] + '"';
 }
 
 // for (var i = 0; i < arquivosAlterados.length; i++) {
