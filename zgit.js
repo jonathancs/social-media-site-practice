f7b88b3f4b0af55c14db4d622d437b964a733c04
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-05-08 22:11:15 -0300

    25. Adding User Profile Photos

--- zgit.js
+++ zgit.js
@@ -1,16 +1,9 @@
 git add ".gitignore"
+git commit -m "25. Adding User Profile Photos"
 git add "controllers/userController.js"
+git commit -m "25. Adding User Profile Photos"
 git add "models/User.js"
+git commit -m "25. Adding User Profile Photos"
 git add "zgit.js"
-
-
-
-
-
-git commit -m "24. User Registration Improvements (Part 2)"
-
-
-
-
-
+git commit -m "25. Adding User Profile Photos"
 git push
