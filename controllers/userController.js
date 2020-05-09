7af6978ec1f48f28d23dc3ce13675e6e37b455e3
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-05-08 22:11:13 -0300

    25. Adding User Profile Photos

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,7 @@ exports.login = function (req, res) {
        ou logando na vigésima vez */
 
     user.login().then(function (result) {
-        req.session.browserUser = {favColor: "blue", username: user.data.username}
+        req.session.user = {avatar: user.avatar, username: user.data.username}
         req.session.save(function () {
             res.redirect('/')
         })
@@ -40,7 +40,7 @@ exports.logout = function (req, res) {
 exports.register = function (req, res) {
     let user = new User(req.body)
     user.register().then(() => {
-        req.session.user = {username: user.data.username}
+        req.session.user = {username: user.data.username, avatar: user.avatar}
         req.session.save(function () {
             res.redirect('/')
         })
@@ -55,10 +55,9 @@ exports.register = function (req, res) {
 }
 
 exports.home = function (req, res) {
-    if (req.session.browserUser) {
-        res.render('home-dashboard', {username: req.session.browserUser.username})
+    if (req.session.user) {
+        res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar})
     } else {
         res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
     }
 }
-
