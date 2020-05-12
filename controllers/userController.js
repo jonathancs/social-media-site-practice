8bcdddcb681f4f5c21da03c3d374cacba70fab43
Jonathan Casagrande <casagrandesoaresj@gmail.com>
2020-05-11 21:12:19 -0300

    3. Post Model (Part 2) - identificando o usuário na postagem que vai para o BD - controllers/userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,7 @@ exports.login = function (req, res) {
        ou logando na vigésima vez */
 
     user.login().then(function (result) {
-        req.session.user = {avatar: user.avatar, username: user.data.username}
+        req.session.user = {avatar: user.avatar, username: user.data.username, _id: user.data._id}
         req.session.save(function () {
             res.redirect('/')
         })
@@ -51,7 +51,7 @@ exports.logout = function (req, res) {
 exports.register = function (req, res) {
     let user = new User(req.body)
     user.register().then(() => {
-        req.session.user = {username: user.data.username, avatar: user.avatar}
+        req.session.user = {username: user.data.username, avatar: user.avatar, _id: user.data._id}
         req.session.save(function () {
             res.redirect('/')
         })
