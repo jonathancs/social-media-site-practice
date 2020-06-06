91dd461174203bb2831a74489d5ca16813e17c3d
jonathancs <casagrandesoaresj@gmail.com>
2020-06-06 20:22:52 -0300

    testando git

--- db.js
+++ db.js
@@ -2,8 +2,4 @@ const dotenv = require('dotenv')
 dotenv.config()
 const mongodb = require('mongodb')
 
-mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function (err, client) {
-    module.exports = client;
-    const app = require('./app')
-    app.listen(process.env.PORT)
-})
+//  comentarios  se eu escreve alguma coisa.
